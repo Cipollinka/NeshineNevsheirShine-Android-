b56b7b7f885cc4dd2b61ea104e1c0f5d3c9ceab5
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-03-27 13:14:01 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import NeshineStack from './StackNavigator';
+
+function App() {
+  return <NeshineStack />;
+}
+
+export default App;
---
+++ src/components/localDelightsData.js
@@ -0,0 +1,36 @@
+export default [
+    {
+        id: 10,
+        title: 'Seten Restaurant Göreme',
+        description: 'A charming restaurant offering traditional Cappadocian dishes in a cozy cave setting.',
+        mapLink: 'https://www.google.com/maps?q=38.6421,34.8287',
+        coordinates: {
+            latitude: 38.6421,
+            longitude: 34.8287,
+        },
+        image: require('../assets/images/neshinePlacesImages/placeImage10.png'),
+    },
+    {
+        id: 11,
+        title: 'Pumpkin Göreme Restaurant',
+        description: 'A family-run eatery known for its warm atmosphere and delicious Turkish cuisine.',
+        mapLink: 'https://www.google.com/maps?q=38.6430,34.8274',
+        rating: 5,
+        coordinates: {
+            latitude: 38.6430,
+            longitude: 34.8274,
+        },
+        image: require('../assets/images/neshinePlacesImages/placeImage11.png'),
+    },
+    {
+        id: 12,
+        title: 'Ürgüp Market',
+        description: 'A lively marketplace where visitors can find local produce, spices, and handmade crafts.',
+        mapLink: 'https://www.google.com/maps?q=38.6293,34.9132',
+        coordinates: {
+            latitude:  38.6293,
+            longitude: 34.9132,
+        },
+        image: require('../assets/images/neshinePlacesImages/placeImage12.png'),
+    },
+]
